93c4b8e5b516b4dabb7cc0d09c5eb8696a05ba27
muneer alkanafani <muneeralkanafani@gmail.com>
2026-01-06 14:42:03 +0300

    add state constructure

--- game_state.py
+++ game_state.py
@@ -1,10 +1,15 @@
 class State:
     """
-    player1 positions (set),
-    player2 positions (set),
-    player turn (bool),
-    sticks (int 1 -> 5)
+    white positions (set),
+    black positions (set),
+    player turn (bool): if it is True the black turn else white turn,
+    sticks (int 1 -> 5),
+    parent (state)
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, white_positions:set = None, black_positions:set = None, turn:bool = True, sticks:int = 0, parent = None):
+        self.white_positions = white_positions if white_positions is not None else set(1, 3, 5, 7, 9, 11, 13)
+        self.black_positions = black_positions if black_positions is not None else set(2, 4, 6, 8, 10, 12, 14)
+        self.black_turn = turn
+        self.sticks = sticks
+        self.parent = parent
