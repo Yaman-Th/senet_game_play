a878fa3e772f61de9fae2dd60c9562c96194a94e
muneer alkanafani <muneeralkanafani@gmail.com>
2026-01-08 18:21:59 +0300

    add player and algorithm mode files

--- game.py
+++ game.py
@@ -1,12 +1,6 @@
-import pygame
 from enum import Enum, auto
-from game_state import State
-from game_renderer.renderer import Renderer
-from game_engine import GameEngine
-from copy import deepcopy
-
-from pygame.math import Vector2
-import time
+from player_mode import PLayerMode
+from algo_player_mode import algorithmPlayerMode
 
 class Mode(Enum):
     PLAYER_MODE = auto()
@@ -15,75 +9,19 @@ class Mode(Enum):
 
 class Game:
     """
-    Class that contain game loop
+    this class guide you to game mode
+    e.g: Game -> PLayer_mode
+              -> algo_player_mode
+              -> algorithm_mode
     """
     def __init__(self, mode:Mode):
-        pygame.init()
-        self.inital_state = State()
-        self.renderer = Renderer()
-        self.engine = GameEngine()
-        
-        self.state = deepcopy(self.inital_state)
-        self.action = None
-    
-        self.start_time =  time.time()
-        
-        # Loop properties
-        self.clock = pygame.time.Clock()
-        self.running = True
-        
-    def processInput(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.running = False
+        self.mode = mode
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    self.running = False
-                elif event.key == pygame.K_z:
-                    self.undo()
-                elif event.key == pygame.K_r:
-                    self.restart()
-                    
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                click_value = self.renderer.handle_events(event.button, self.state.sticks)
-                
-                if click_value == None:
-                    return
-                    
-                elif click_value == -1:
-                    sticks = self.engine.TossStick()
-                    self.state.sticks = sticks
-                    self.engine.handle_special_houses(self.state)
-                else:
-                    self.action = click_value
-                
-                      
-    def restart(self):
-        self.state = self.inital_state
-    
-    def update(self):
-        new_state = self.engine.transition_model(self.state, self.action)
-        if new_state is None:
-            return
-        
-        self.state = new_state
-        self.action = None
-        
-                 
-    def render(self):
-        self.renderer.render(self.state, self.start_time)
-    
     def run(self):
-        while self.running:
-            
-            self.processInput()
-            self.update()
-            self.render()
-            self.clock.tick(60)
-        
-        self.elapsed_time = time.time() - self.start_time
-        # print(f"Moves: {self.moveCount}")
-        print(f"Time: {self.elapsed_time:.2f}s")
-        pygame.quit()
-        exit()
+        if self.mode == Mode.PLAYER_MODE:
+            game_loop = PLayerMode()
+        elif self.mode == Mode.ALGORITHM_AND_PLAYER_MODE:
+            game_loop = algorithmPlayerMode()
+        elif self.mode == Mode.ALGORITHM_MODE:
+            game_loop = PLayerMode()
+        game_loop.run()
