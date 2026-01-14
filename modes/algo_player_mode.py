884917004d144517d4839506c18015cb0bae7fe7
muneer alkanafani <muneeralkanafani@gmail.com>
2026-01-14 23:17:47 +0300

    refactor the modes classes and game loop

--- modes/algo_player_mode.py
+++ modes/algo_player_mode.py
@@ -1,66 +1,31 @@
-import time
 import pygame
-from copy import deepcopy
-from game_engine import GameEngine
-from game_state import PlayerColor, State
-from game_renderer.renderer import Renderer
+from game_state import PlayerColor
 from algorithms.expectiminimax import ExpectiMinimaxPlayer
 
 class algorithmPlayerMode:
     """Class that contain game loop (algorithm vs player)"""
-    def __init__(self):
-        pygame.init()
+    def __init__(self, game):
+        self.game = game
         self.algo = ExpectiMinimaxPlayer(PlayerColor.BLACK)
-        self.renderer = Renderer()
-        self.engine = GameEngine()
-        self.inital_state = State()
-        self.state = deepcopy(self.inital_state)
-        self.action = None
-        self.waiting_for_sticks = False
-        # Loop properties
-        self.clock = pygame.time.Clock()
-        self.running = True
-        self.start_time =  time.time()
         
-    def processInput(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.running = False
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    self.running = False
-                elif event.key == pygame.K_z:
-                    self.undo()
-                elif event.key == pygame.K_r:
-                    self.restart()
-
-            if self.state.current_player == PlayerColor.WHITE:
+    def processInput(self, events):
+        if self.game.state.current_player == PlayerColor.WHITE:
+            for event in events:
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                        click_value = self.renderer.handle_events(event.button, self.state.sticks, self.state, self.engine.actions(self.state))
-                        if click_value == None:
-                            return
-                        elif click_value == -1:
-                            sticks = self.engine.TossStick()
-                            self.state.sticks = sticks
-                            self.engine.handle_special_houses(self.state)
-                        else:
-                            self.action = click_value 
-            else:
-                pass
-                      
-    def restart(self):
-        self.state = self.inital_state
-        self.state.sticks = 0
-        self.action = None
-        self.waiting_for_sticks = False
-
-    def undo(self):
-        if self.state.parent is not None:
-            self.state = self.state.parent
-            self.state.sticks = 0
-            self.action = None
-            self.waiting_for_sticks = False
+                    click_value = self.game.renderer.handle_events(
+                        event.button,
+                        self.game.state.sticks,
+                        self.game.state,
+                        self.game.engine.actions(self.game.state)
+                    )
+                    if click_value is None:
+                        continue
+                    elif click_value == -1:
+                        sticks = self.game.engine.TossStick()
+                        self.game.state.sticks = sticks
+                        self.game.engine.handle_special_houses(self.game.state)
+                    else:
+                        self.game.action = click_value 
     
     def update(self):
         if self.game.state.current_player == PlayerColor.BLACK:
