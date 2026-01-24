ee1607d4418f44ab0ed8d0c93fb4b03c16926c56
Yaman-Th <tahhan152@gmail.com>
2026-01-24 08:14:49 +0300

    add tracking panel for ai information

--- modes/algo_mode.py
+++ modes/algo_mode.py
@@ -2,47 +2,38 @@ from game_state import PlayerColor
 # from algorithms.expectiminimax import ExpectiMinimaxPlayer
 from algorithms.algorithm import ExpectiMinimaxPlayer
 import random
+from game_renderer.renderer import Renderer 
 from copy import deepcopy
 
 class algorithmMode:
     """Class that contain game loop (algorithm vs algorithm)"""
     def __init__(self, game):
         self.game = game
-        self.algo_black = ExpectiMinimaxPlayer(4)
-        self.algo_white = ExpectiMinimaxPlayer(4)
+        self.renderer = Renderer()
+        self.algo_black = ExpectiMinimaxPlayer(2)
+        self.algo_white = ExpectiMinimaxPlayer(2)
+        self.sticks = None
+        self.turn = None
+        self.actions = None
+        self.best_action = None
         
     def processInput(self, events):
         pass
     
     def update(self):
         if self.game.state.current_player == PlayerColor.BLACK:
+            self.turn = "Black"
             self._algorithm_turn(self.algo_black)
         elif self.game.state.current_player == PlayerColor.WHITE:
+            self.turn = "White"
             self._algorithm_turn(self.algo_white)
-            # self._random_agent_turn()
 
+    def render(self):
+        self.renderer.render(self.game.state, self.actions, self.sticks, self.turn, self.best_action, self.algo_black.stats() if self.turn == "Black" else self.algo_white.stats())
+    
     def _algorithm_turn(self, ai_player):
         sticks = self.game.engine.TossStick()
-        self.game.state.sticks = sticks
-        self.game.engine.handle_special_houses(self.game.state)
-
-        if not self.game.engine.actions(self.game.state):
-            self.game.state.change_player()
-            return
-        
-        ai_state = deepcopy(self.game.state)
-        
-        best_action = ai_player.best_action(ai_state)
-        
-        if best_action and best_action is not None:
-            self.game.action = best_action
-            new_state = self.game.engine.transition_model(self.game.state, self.game.action)
-            if new_state is not None:
-                self.game.state = new_state
-                self.game.action = None
-    
-    def _random_agent_turn(self):
-        sticks = self.game.engine.TossStick()
+        self.sticks = sticks
         self.game.state.sticks = sticks
         self.game.engine.handle_special_houses(self.game.state)
 
@@ -51,12 +42,16 @@ class algorithmMode:
             self.game.state.change_player()
             return
         
-        # best_action = ai_player.find_best_move(self.game.state)
-        action = random.choice(list(actions))
+        self.actions = actions
+        ai_state = deepcopy(self.game.state)
         
-        if action and action is not None:
-            self.game.action = action
+        best_action = ai_player.best_action(ai_state)
+        self.best_action = best_action
+        
+        if best_action and best_action is not None:
+            self.game.action = best_action
             new_state = self.game.engine.transition_model(self.game.state, self.game.action)
             if new_state is not None:
                 self.game.state = new_state
                 self.game.action = None
+                self.best_action = None
